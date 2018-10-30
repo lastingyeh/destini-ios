12bbd51114cec1a33adc018a4b6cb1e0c7c985c9
chrisyeh <lastingyeh@gmail.com>
2018-10-30 11:46:49 +0800

    init

---
+++ Destini/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Philipp Muellauer on 01/09/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Our strings
+    let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
+    let answer1a = "I\'ll hop in. Thanks for the help!"
+    let answer1b = "Better ask him if he\'s a murderer first."
+    
+    
+    let story2 = "He nods slowly, unphased by the question."
+    let answer2a = "At least he\'s honest. I\'ll climb in."
+    let answer2b = "Wait, I know how to change a tire."
+    
+    let story3 = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
+    let answer3a = "I love Elton John! Hand him the cassette tape."
+    let answer3b = "It\'s him or me! You take the knife and stab him."
+    
+    let story4 = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
+    let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
+    let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
+    
+    
+    // UI Elements linked to the storyboard
+    @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
+    @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
+    @IBOutlet weak var storyTextView: UILabel!
+    
+    // TODO Step 5: Initialise instance variables here
+    var storyIndex: Int = 0
+    var currentStory: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
+        updateUI(story:story1, answer1a, answer1b, isHidden: false)
+    }
+    
+    // User presses one of the buttons
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        storyIndex += 1
+        
+        displayLayer(tag: sender.tag)
+    }
+    
+    func updateUI(story: String, _ answerA: String = "", _ answerB: String = "", isHidden: Bool){
+        storyTextView.text = story
+        
+        if isHidden {
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+        } else {
+            topButton.setTitle(answerA, for: .normal)
+            bottomButton.setTitle(answerB, for: .normal)
+        }
+    }
+    
+    func displayLayer(tag: Int){
+        switch storyIndex {
+        case 1:
+            if tag == 1 {
+                updateUI(story: story3, answer3a, answer3b, isHidden: false)
+                currentStory = "story3"
+            } else if tag == 2 {
+                updateUI(story: story2, answer2a, answer2b, isHidden: false)
+                currentStory = "story2"
+            }
+        case 2:
+            if currentStory == "story3" {
+                if tag == 1 {
+                    updateUI(story: story6, isHidden: true)
+                } else if tag == 2 {
+                    updateUI(story: story5, isHidden: true)
+                }
+            } else if currentStory == "story2" {
+                if tag == 1 {
+                    updateUI(story: story3, answer3a, answer3b, isHidden: false)
+                    currentStory = "story3"
+                } else if tag == 2 {
+                    updateUI(story: story4, isHidden: true)
+                }
+            }
+        case 3:
+            if currentStory == "story3" {
+                if tag == 1 {
+                    updateUI(story: story6, isHidden: true)
+                } else if tag == 2 {
+                    updateUI(story: story5, isHidden: true)
+                }
+            }
+        default:
+            updateUI(story: story1, answer1a, answer1b, isHidden: false)
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,25 @@
+### My Swift Course notes (Destini-Story-Outline-and-Story-Structure)
+
+UI
+1. UIButton
+2. UILabel
+
+Event
+1. IBAction
+2. 
+
+Syntax
+1. if ... else
+2. switch
+3. func params usage
+
+### Display
+
+
+
+
+### all resources and copyright by   
+
+[1. Github Destini-iOS12](https://github.com/londonappbrewery/Destini-iOS12)
+
+[2. Udemy iOS 12 & Swift - The Complete iOS App Development Bootcamp](https://www.udemy.com/ios-12-app-development-bootcamp/)
